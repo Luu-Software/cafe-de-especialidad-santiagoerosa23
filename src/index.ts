da366d844c25f55eccdc349780d882a8940f1a1e
santiagoerosa23 <santiago.erosa23@gmail.com>
2026-05-26 12:16:21 -0300

    falta que el resultado de bien

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,11 @@ let totalTexto: string = preguntar('Total de la cuenta: ');
 let esEstudiante: string = preguntar('¿Son estudiantes de ORT? si/no: ');
 let personasTexto: string = preguntar('Dividir la cuenta entre: ');
 
-let totalPorPersona: number = 0; // COMPLETAR
+let totalPorPersona: number = Number(totalTexto) / Number(personasTexto); // COMPLETAR
+if (esEstudiante==="si"){
+   totalPorPersona*0.90
+}
+if (esEstudiante==="no")
+
+
 console.log(`Total por persona: $${totalPorPersona}`);
